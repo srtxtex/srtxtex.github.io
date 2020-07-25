7a35c0f9db010795778bf047245ab7503c35449a
VirtusTex <srtxtex@gmail.com>
2020-07-26 02:58:18 +0300

    try 2 react

---
+++ portfolio/mera-react/precache-manifest.96839c0eef27d724665b21d0b0955852.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b9f913ace6be4f778ae737ed8f75e63",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0d4331a1df534a430544",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "aef95435a667b17ff89e",
+    "url": "/static/js/2.1927a3e5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.1927a3e5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0d4331a1df534a430544",
+    "url": "/static/js/main.2f90c1ab.chunk.js"
+  },
+  {
+    "revision": "72fb3100343db6adf75e",
+    "url": "/static/js/runtime-main.5d5186b0.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
